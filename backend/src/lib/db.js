86b1f5ac2eb378a805865b513daba5c44d14c2cb
subramani2002 <subramanisubash123@gmail.com>
2026-02-21 19:14:51 +0530

    updated handling of databse connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import { ENV } from './env.js';
 
 export const connectDB = async() => {
     try {
+        if(!ENV.DB_URL) {
+            throw new Error("Database URL is not defined in environment variables");
+        }
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("Connected to database successfully");
     } catch (error) {
